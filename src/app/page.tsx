fdc66b1fbe680d2e23648e7391803029400e4c9b
Jessy Hanifiah <jessyhanifiah@gmail.com>
2023-12-20 10:57:45 +0700

    chore: setup boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center space-y-4">
+      <h1 className=" text-4xl font-bold">Indonesia Open Course</h1>
+    </div>
+  );
+}
