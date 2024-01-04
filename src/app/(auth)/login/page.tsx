41e208797d6c5179e4487704e95fe962d125f18a
Jessy Hanifiah <jessyhanifiah@gmail.com>
2024-01-04 15:55:33 +0700

    feat: slice login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import { Login } from '@/components/(auth)/auth/login';
+
+export default function Page() {
+  return <Login />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Login } from '@/components/(auth)/auth/login';
+
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-4">
-      <h1 className=" text-4xl font-bold">Indonesia Open Course</h1>
+      <Login />
     </div>
   );
 }
